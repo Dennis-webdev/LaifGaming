7a7a59924df22ca827be540791379139821e2cfb
Dennis-webdev <dennisrichter.business@gmail.com>
2020-10-29 23:19:48 +0100

    dev

--- src/index.js
+++ src/index.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Link 
+} from 'react-router-dom';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/" component={Home} />
+
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    Hello World!
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.querySelector("#root")
 );
